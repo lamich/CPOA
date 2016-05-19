db318936d7f444709bc37d91d65dbf99a2b4bfc8
cyrille <cyrillemichard38@gmail.com>
2016-05-19 10:32:44 +0200

    ajout mysql connextor

--- Application/src/ihm/FenetreApplication.java
+++ Application/src/ihm/FenetreApplication.java
@@ -39,36 +39,114 @@ public class FenetreApplication extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        laTable = new javax.swing.JTable();
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanelVip = new javax.swing.JPanel();
+        laTableVip = new javax.swing.JTable();
+        btAjouterVip = new javax.swing.JButton();
+        btSupprimerVip = new javax.swing.JButton();
+        jPanelEvenement = new javax.swing.JPanel();
+        jTable1 = new javax.swing.JTable();
+        jPanelPhoto = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         setTitle("Affichage des employés");
+        setPreferredSize(new java.awt.Dimension(800, 550));
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 formWindowClosing(evt);
             }
         });
 
-        laTable.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        laTable.setModel(leModele);
-        jScrollPane1.setViewportView(laTable);
+        laTableVip.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        laTableVip.setModel(leModele);
+        laTableVip.setPreferredSize(new java.awt.Dimension(500, 450));
+
+        btAjouterVip.setText("Ajpouter");
+        btAjouterVip.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btAjouterVipActionPerformed(evt);
+            }
+        });
+
+        btSupprimerVip.setText("Supprimer");
+
+        javax.swing.GroupLayout jPanelVipLayout = new javax.swing.GroupLayout(jPanelVip);
+        jPanelVip.setLayout(jPanelVipLayout);
+        jPanelVipLayout.setHorizontalGroup(
+            jPanelVipLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelVipLayout.createSequentialGroup()
+                .addGap(43, 43, 43)
+                .addGroup(jPanelVipLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btAjouterVip)
+                    .addComponent(btSupprimerVip))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 91, Short.MAX_VALUE)
+                .addComponent(laTableVip, javax.swing.GroupLayout.PREFERRED_SIZE, 600, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanelVipLayout.setVerticalGroup(
+            jPanelVipLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelVipLayout.createSequentialGroup()
+                .addGroup(jPanelVipLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanelVipLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(laTableVip, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanelVipLayout.createSequentialGroup()
+                        .addGap(170, 170, 170)
+                        .addComponent(btAjouterVip)
+                        .addGap(70, 70, 70)
+                        .addComponent(btSupprimerVip)))
+                .addContainerGap(39, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("VIP", jPanelVip);
+
+        javax.swing.GroupLayout jPanelEvenementLayout = new javax.swing.GroupLayout(jPanelEvenement);
+        jPanelEvenement.setLayout(jPanelEvenementLayout);
+        jPanelEvenementLayout.setHorizontalGroup(
+            jPanelEvenementLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelEvenementLayout.createSequentialGroup()
+                .addContainerGap(252, Short.MAX_VALUE)
+                .addComponent(jTable1, javax.swing.GroupLayout.PREFERRED_SIZE, 563, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanelEvenementLayout.setVerticalGroup(
+            jPanelEvenementLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelEvenementLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jTable1, javax.swing.GroupLayout.PREFERRED_SIZE, 457, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(32, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("Evenement", jPanelEvenement);
+
+        javax.swing.GroupLayout jPanelPhotoLayout = new javax.swing.GroupLayout(jPanelPhoto);
+        jPanelPhoto.setLayout(jPanelPhotoLayout);
+        jPanelPhotoLayout.setHorizontalGroup(
+            jPanelPhotoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 825, Short.MAX_VALUE)
+        );
+        jPanelPhotoLayout.setVerticalGroup(
+            jPanelPhotoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 500, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("Photo", jPanelPhoto);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(204, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 600, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jTabbedPane1)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(39, 39, 39)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(100, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(jTabbedPane1)
+                .addContainerGap())
         );
 
         pack();
@@ -82,8 +160,26 @@ public class FenetreApplication extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_formWindowClosing
 
+    private void btAjouterVipActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btAjouterVipActionPerformed
+        try {
+            VIP vip = new VIP();
+            FenetreSaisie laSaisie = new FenetreSaisie(this, vip);
+            if (laSaisie.doModal() == true) {
+               
+            }
+        } catch (Exception e) {
+            System.out.println("Exception à l'insertion : " + e.getMessage());
+        }
+    }//GEN-LAST:event_btAjouterVipActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable laTable;
+    private javax.swing.JButton btAjouterVip;
+    private javax.swing.JButton btSupprimerVip;
+    private javax.swing.JPanel jPanelEvenement;
+    private javax.swing.JPanel jPanelPhoto;
+    private javax.swing.JPanel jPanelVip;
+    private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTable laTableVip;
     // End of variables declaration//GEN-END:variables
 }
