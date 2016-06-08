2e8d89e5300c7eebac916a44185f07add5d8774e
cyrille <cyrillemichard38@gmail.com>
2016-06-08 17:13:25 +0200

    sup import

--- Application/src/modele/ModeleJTableEvenement.java
+++ Application/src/modele/ModeleJTableEvenement.java
@@ -9,8 +9,6 @@ import accesAuxDonnees.DaoEvenement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.table.AbstractTableModel;
 import metier.Evenement;
 
